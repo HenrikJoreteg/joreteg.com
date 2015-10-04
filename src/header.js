32142f109665e26c1379315e0b4f8b169e79b055
Henrik Joreteg <hjoreteg@gmail.com>
2015-10-03 23:37:17 -0700

    fix broken links

--- src/header.js
+++ src/header.js
@@ -17,7 +17,7 @@ export default class Header extends Component {
         <img className='bannerImage' src={imageUrl}/>
         <h2>{this.props.title}</h2>
         <p>{this.props.subtitle}</p>
-        <p><a href='/blog/all'>all posts</a> | <a href='http://twitter.com/henrikjoreteg'>twitter</a> | <a href='mailto:henrik@joreteg.com'>email</a> | <a href='consulting@joreteg.com'>hire</a> | <a href='http://humanjavascript.com'>book</a> | <a href='http://learn.humanjavascript.com'>tutorials</a></p>
+        <p><a href='/blog/all'>all posts</a> | <a href='http://twitter.com/henrikjoreteg'>twitter</a> | <a href='mailto:henrik@joreteg.com'>email</a> | <a href='http://consulting.joreteg.com'>hire</a> | <a href='http://humanjavascript.com'>book</a> | <a href='http://learn.humanjavascript.com'>tutorials</a></p>
       </header>
     )
   }
